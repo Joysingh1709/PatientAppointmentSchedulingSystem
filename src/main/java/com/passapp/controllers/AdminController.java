368d1c14de4f55104d0262e38b9f436f0a5113f2
Joy Singh <avnishmamta060606@gmail.com>
2022-03-17 17:21:07 +0530

    admin controller added for html testing

--- src/main/java/com/passapp/controllers/AdminController.java
+++ src/main/java/com/passapp/controllers/AdminController.java
@@ -1,5 +1,18 @@
 package com.passapp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping(value = "/admin", method = RequestMethod.GET)
 public class AdminController {
+
+	@GetMapping()
+	public ModelAndView adminHome() {
+		return new ModelAndView("admin");
+	}
 
 }
