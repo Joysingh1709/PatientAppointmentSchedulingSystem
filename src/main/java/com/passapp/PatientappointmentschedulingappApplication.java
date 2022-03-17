be2cda7cd9d74e2fb00ba44b053b0bf3ba7e8604
Joy Singh <avnishmamta060606@gmail.com>
2022-03-17 13:08:45 +0530

    configure project

---
+++ src/main/java/com/passapp/PatientappointmentschedulingappApplication.java
@@ -0,0 +1,13 @@
+package com.passapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientappointmentschedulingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientappointmentschedulingappApplication.class, args);
+	}
+
+}
