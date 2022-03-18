b87e1e3c705efbc61f1d3d9a7d9f766ae75031de
KrutikaMarotkar <marotkarkrutika@gmail.com>
2022-03-18 17:49:14 +0530

    login controller updated

--- src/main/java/com/passapp/repository/DoctorLoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.passapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.passapp.models.Doctor;
-
-public interface DoctorLoginRepository extends JpaRepository<Doctor,Long>{
-
-}
--- src/main/java/com/passapp/repository/ReceptionistLoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.passapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.passapp.models.Receptionist;
-
-public interface ReceptionistLoginRepository extends JpaRepository<Receptionist,Long> {
-
-}
