f0e5425f1e8613f33a6fe00aff9e87c8185f30a0
Aparna Paul <aparna2000paul@gmail.com>
2022-03-18 12:48:06 +0530

    Controller done

--- src/main/java/com/passapp/controllers/AppointmentController.java
+++ src/main/java/com/passapp/controllers/AppointmentController.java
@@ -1,5 +1,16 @@
 package com.passapp.controllers;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.passapp.exceptions.AppointmentNotConfirmException;
+
+@RestControllerAdvice
 public class AppointmentController {
+	
+	@ExceptionHandler(AppointmentNotConfirmException.class)
+	public String AppointmentNotConfirmHaldler(AppointmentNotConfirmException ax) {
+		return ax.getMessage();
+	}
 
 }
--- src/main/java/com/passapp/controllers/PatientController.java
+++ src/main/java/com/passapp/controllers/PatientController.java
@@ -1,5 +1,16 @@
 package com.passapp.controllers;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.passapp.exceptions.PatientNotFoundException;
+
+@RestControllerAdvice
 public class PatientController {
 
+	@ExceptionHandler(PatientNotFoundException.class)
+	public String patientNotFoundHandler(PatientNotFoundException px) {
+		return px.getMessage();
+	}
+	
 }
