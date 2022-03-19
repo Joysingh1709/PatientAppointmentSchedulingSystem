08bdbf6f918318c2ff6f9596ca5a3c4dfbcc0d3e
KrutikaMarotkar <marotkarkrutika@gmail.com>
2022-03-19 13:45:39 +0530

    login page

--- src/main/java/com/passapp/controllers/LoginController.java
+++ src/main/java/com/passapp/controllers/LoginController.java
@@ -3,14 +3,22 @@ package com.passapp.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.passapp.models.User;
-@RequestMapping("/login")
+@RestController
+
 public class LoginController {
-	@GetMapping("/")
+	@GetMapping("/registration")
 	public ModelAndView getPatientForm(@ModelAttribute User user) {
-		ModelAndView modelAndView = new ModelAndView("patientform");
+		ModelAndView modelAndView = new ModelAndView("patientregistration");
+		return modelAndView;
+	}
+	
+	@GetMapping("/login")
+	public ModelAndView getLoginPage(@ModelAttribute User user) {
+		ModelAndView modelAndView = new ModelAndView("LoginPage");
 		return modelAndView;
 	}
 }
