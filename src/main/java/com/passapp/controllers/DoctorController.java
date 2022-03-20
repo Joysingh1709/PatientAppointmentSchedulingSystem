208c91264aa745c1de7bfe60e76066a40e6a676c
Joy Singh <avnishmamta060606@gmail.com>
2022-03-20 21:53:24 +0530

    PostData.js

--- src/main/java/com/passapp/controllers/DoctorController.java
+++ src/main/java/com/passapp/controllers/DoctorController.java
@@ -1,5 +1,24 @@
 package com.passapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.passapp.models.Doctor;
+import com.passapp.services.DoctorService;
+
+@RestController
+@RequestMapping("/doctor")
 public class DoctorController {
+
+	@Autowired
+	private DoctorService doctorService;
+
+	@GetMapping()
+	public Doctor getDoctor(@ModelAttribute Doctor doctor) {
+		doctorService
+	}
 
 }
--- src/main/resources/static/mainjs.js
+++ src/main/resources/static/mainjs.js
@@ -1,3 +1,5 @@
+const postData = require('./PostData');
+
 /**
  * Doctor's Registration form validattions
  */
@@ -167,17 +169,5 @@ function onPatRegister() {
 		patpassword.classList.add("is-invalid");
 	}
 
-}
-
-async function postData(url = '', data = {}) {
-	const response = await fetch(url, {
-		method: 'POST', // *GET, POST, PUT, DELETE, etc.
-		headers: {
-			'Content-Type': 'application/json'
-			// 'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		body: JSON.stringify(data) // body data type must match "Content-Type" header
-	});
-	return response.json(); // parses JSON response into native JavaScript objects
 }
 
