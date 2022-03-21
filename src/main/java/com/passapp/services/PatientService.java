5938f4024e312c5dfd82801d10ecafc7cb921152
PiyushaPaul <piyushapaul1999@gmail.com>
2022-03-21 13:32:04 +0530

    solve error

--- src/main/java/com/passapp/services/PatientService.java
+++ src/main/java/com/passapp/services/PatientService.java
@@ -8,7 +8,11 @@ import com.passapp.models.User;
 public interface PatientService {
     
 	
+	
+	
+	
 	public User getPatient(String email, String password ) throws PatientNotFoundException;
+	
 	
 	//CRUD Operations
 	public User addPatient(User user);
@@ -20,4 +24,5 @@ public interface PatientService {
 	public boolean deletePatient(User patient);
 			
 	public boolean updateUser(User user);
+	
 }
