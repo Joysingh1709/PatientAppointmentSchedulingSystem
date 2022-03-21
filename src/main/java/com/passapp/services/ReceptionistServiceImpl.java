75e6de7109d27889a5eb7794dfd5715ca4b459fe
PiyushaPaul <piyushapaul1999@gmail.com>
2022-03-21 11:23:53 +0530

    Crud Operations in Service Class

--- src/main/java/com/passapp/services/ReceptionistServiceImpl.java
+++ src/main/java/com/passapp/services/ReceptionistServiceImpl.java
@@ -1,5 +1,7 @@
 package com.passapp.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +16,45 @@ public class ReceptionistServiceImpl implements ReceptionistService {
 
 	@Override
 	public Receptionist addReceptionist(Receptionist receptionist) {
-		// TODO Auto-generated method stub
 		return receptionistRepository.save(receptionist);
+	}
+
+	@Override
+	public Receptionist getReceptionistById(Long receptionistId) {
+		Optional<Receptionist> receptionist = receptionistRepository.findById(receptionistId);
+		if(receptionist.isPresent()) {
+			return receptionist.get();
+		}
+		return null;
+	}
+
+	@Override
+	public boolean deleteReceptionistById(Long receptionistId) {
+		receptionistRepository.deleteById(receptionistId);
+		if(receptionistRepository.existsById(receptionistId)) {
+		return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean deleteReceptionist(Receptionist receptionist) {
+		receptionistRepository.delete(receptionist);
+		if(receptionistRepository.existsById(receptionist.getReceptionistId())){
+		     return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean updateReceptionist(Receptionist receptionist) {
+		if(receptionistRepository.existsById(receptionist.getReceptionistId())) {
+			Receptionist rec= receptionistRepository.save(receptionist);
+			if(rec!=null) {
+			return true;
+			}
+		}
+		return false;
 	}
 
 }
